2139f14d5d93bbeb1d54d9c9c24ed1068336bc53
Jake Stambaugh <jake.stambaugh@gmail.com>
2020-01-28 23:01:10 -0500

    Add formatting to the lecture parsing and remove the storage component

--- storage.py
+++ storage.py
@@ -1,15 +1,28 @@
-from google.cloud import storage, datastore
+from google.cloud import datastore
 
-def create_storage_client():
-    return storage.Client()
 
-def get_metadata(blob):
-    return {"date": blob.updated, "title": blob.id, "url": blob.public_url}
+def create_datastore_client():
+    """ This method lives in the "storage" package, so I don't have to import
+    all of the google.cloud packages in my main.py
 
-def list_slides(storage_client):
+    In this project it isn't a huge concern, but in larger projects it is good
+    to separate dependencies this way. If I want to change how I store data
+    in the future, I can refactor these methods without changing the rest of my
+    code.
+    """
+    return datastore.Client()
+
+
+def create_view(metadata):
+    print(metadata)
+    return {"date": metadata["presented_at"], "title": metadata["title"], "url": metadata["public_url"]}
+
+
+def list_slides(datastore_client):
     """ Reads all of the slides from the storage client
     """
-    bucket = storage_client.get_bucket("jake1520-slides")
-    blobs = storage_client.list_blobs(bucket)
-    
-    return [get_metadata(blob) for blob in blobs]
+    query = datastore_client.query(kind="Lecture")
+    lectures = query.fetch()
+
+    slideshows = [create_view(metadata) for metadata in lectures]
+    return sorted(slideshows, key=lambda i: i["date"])
